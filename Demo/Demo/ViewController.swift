a48df43f28991f6c12c5c5f3a8e735a741d709cc
LEE ZHE YU <tzef8220@gmail.com>
2022-06-19 16:13:30 +0800

    Set up local search dependency and UI module SPM

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -4,14 +4,18 @@
 //
 
 import UIKit
+import ModuleUI
+import ModuleSearch
 
 class ViewController: UIViewController {
+    let searchController = ModuleSearch.UIFactory.searchController(
+        designSystemFactory: ModuleUI.DesignSystemFactory()
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        searchController.setup(navigationItem: navigationItem)
     }
-
-
 }
 
